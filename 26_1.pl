b846abb766b8657424846312c4d6c60476c10f5a
Unknown <arseniyzhuck@mail.ru>
2020-03-23 08:24:05 +0300

    pr3_1
    read_list, sum_list

--- 26_1.pl
+++ 26_1.pl
@@ -61,3 +61,18 @@ n_pr(X,N):-n_pr(X,X,N).
 n_pr(X,K,K):-Ost is X mod K,Ost=0,pr(K),!.
 n_pr(X,K,N):- K1 is K-1, n_pr(X,K1,N).
 
+pr3_1:-read(N),read_list(A,N),sum_list(A,Sum),write(Sum).
+
+read_list(A,N):-read_list([],A,0,N).
+read_list(A,A,N,N):-!.
+read_list(List,A,I,N):-	I1 is I+1,read(X),append(List,[X],List1),
+						read_list(List1,A,I1,N).
+
+sum_list(A,Sum):-sum_list(A,0,Sum).
+sum_list([],Sum,Sum):-!.
+sum_list([H|T],S,Sum):-S1 is S+H,sum_list(T,S1,Sum).
+
+						
+						
+
+
